7ba94eedbf80bb77567d2b2340a0e9ce89436cb0
jotnguyen <johntnguyen9@gmail.com>
2019-01-23 12:53:07 -0600

    working on csvs

--- app/App.js
+++ app/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {Text, View , Button} from 'react-native';
 import * as d3 from 'd3';
+import * as Papa from 'papaparse';
+// import neighborhood from './neighborhood_zipcodes.csv';
+import * as RNFS from 'react-native-fs';
+
 
 class HelloReactNative extends Component {
     constructor(props){
@@ -10,6 +14,36 @@ class HelloReactNative extends Component {
       }
     }
     parseSQL = () => {
+      var mainBundlePath = RNFS.MainBundlePath;
+      console.log("dat bundle tho",mainBundlePath)
+      RNFS.readDir(mainBundlePath) // On Android, use "RNFS.DocumentDirectoryPath" (MainBundlePath is not defined)
+	.then((result) => {
+	  console.log('GOT RESULT', result[4]);
+	  //	  this.setState({neighborhood: result[4]});
+	  return Papa.parse(result[4].path, {
+	    download: false,
+	    delimiter: '\t',
+	    complete: function (results){
+	      console.log('ressssults',results);
+	    }
+	  });
+	}).
+	then( (result) => {
+	  console.log(result);
+	})
+        .catch(err => console.log("err",err));
+
+      // Papa.parse(mainBundlePath, {
+      // 	download: true,
+      // 	worker: true, // Don't bog down the main thread if its a big file
+      // 	step: function(result) {
+      //   // do stuff with result
+      // 	},
+      // 	complete: function(results, file) {
+      //     console.log('parsing complete read', count, 'records.'); 
+      // 	}
+//    });
+
       //console.log(hoodDate);
       // d3.csvParse("/neighborhood_zipcodes.csv").
       // 	then((data) => {
